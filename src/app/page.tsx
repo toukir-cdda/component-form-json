642eb79358bbb0410a4f183cf0e925430b0ec0c7
Toukir Ahmed Raju <toukir@cdda.io>
2023-10-18 14:25:32 +0600

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,109 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+import { createUseStyles } from "react-jss";
+import { compoData } from "../../data";
+import { create } from "jss";
+import preset from "jss-preset-default";
 
 export default function Home() {
+  const { templates, available_media_devices } = compoData;
+  const jss = create();
+  jss.setup(preset());
+
+  // const sheet = jss.createStyleSheet({
+
+  //   [`@media (min-width: ${minWidth}px)`]: {
+  //       color: 'red',
+  //     },
+
+  //   }
+  // }).attach();
+  //find parent children
+  const modifiedData = (templates) => {
+    const modifiedData = [];
+
+    for (const template of templates) {
+      const { elements, ...others } = template;
+
+      const findChildren = (parentId) => {
+        const children = [];
+        for (const item of elements) {
+          if (item.parent_id === parentId) {
+            const childItem = { ...item, children: findChildren(item.id) };
+            children.push(childItem);
+          }
+        }
+        return children.length > 0 ? children : null;
+      };
+
+      for (const item of elements) {
+        if (!item.parent_id) {
+          const newItem = {
+            ...item,
+            ...others,
+            children: findChildren(item.id),
+          };
+          modifiedData.push(newItem);
+        }
+      }
+    }
+
+    return modifiedData;
+  };
+
+  const newModifiedData = modifiedData(templates);
+
+  const renderComponent = (component) => {
+    const {
+      id,
+      tag: Tag,
+      attributes,
+      dynamic_content,
+      static_content,
+      children,
+    } = component;
+    const { styleMedia } = attributes;
+    const { desktop, laptop, mobile, tablet } = styleMedia;
+    // console.log(styleMedia);
+    const dex = jss
+      .createStyleSheet({
+        // //desktop
+        "@global": {
+          body: {
+            margin: 0,
+            padding: 0,
+          },
+        },
+        [`@media (min-width: ${available_media_devices.styleMedia.desktop.minResulation}) `]:
+          styleMedia.desktop,
+        //laptop
+        [`@media (min-width: ${available_media_devices.styleMedia.laptop.minResulation}) and (max-width: ${available_media_devices.styleMedia.laptop.maxResulation})`]:
+          styleMedia.laptop,
+        //mobile
+        [`@media (max-width: ${available_media_devices.styleMedia.mobile.maxResulation})`]:
+          styleMedia.mobile,
+        //tablet
+        [`@media (min-width: ${available_media_devices.styleMedia.tablet.minResulation}) and (max-width: ${available_media_devices.styleMedia.tablet.maxResulation})`]:
+          styleMedia.tablet,
+      })
+      .attach();
+    // const sheet = jss.createStyleSheet(styleMedia.tablet).attach();
+    console.log(dex);
+    return (
+      <>
+        {Tag === "img" ? (
+          <Tag key={id} {...attributes} className={dex.classes.style} />
+        ) : (
+          <Tag {...attributes} key={id} className={dex.classes.style}>
+            {static_content}
+            {/* {component?.template_author} */}
+            {children && children.map((child) => renderComponent(child))}
+          </Tag>
+        )}
+      </>
+    );
+  };
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    <div> {newModifiedData.map((component) => renderComponent(component))}</div>
+  );
 }
